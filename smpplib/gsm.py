a01c7a28ddbbe590ace0200093ebd963fd25eb9b
Javier del Egido <javier.egido@edu.uah.es>
2025-08-22 10:40:26 +0000

    Adaptación SMPP fragmentado manualmente

--- smpplib/gsm.py
+++ smpplib/gsm.py
@@ -8,35 +8,49 @@ from smpplib import consts, exceptions
 
 def make_parts(text, encoding=consts.SMPP_ENCODING_DEFAULT, use_udhi=True):
     """Returns tuple(parts, encoding, esm_class)"""
+
+    encoded_text = []
     try:
         # Try to encode with the user-defined encoding first.
         encode, split_length, part_size = ENCODINGS[encoding]
-        encoded_text = encode(text)
+        if "*fragmento*" in text:
+            text_split = text.split("*fragmento*")
+            for part in text_split:
+                encoded_text.append(encode(part))
+        else:
+            encoded_text = encode(text)
     except KeyError:
         raise NotImplementedError('encoding is not supported: %s' % encoding)
-    except UnicodeError:
+    """ except UnicodeError:
         # Fallback to UCS-2.
         encoding = consts.SMPP_ENCODING_ISO10646
         encode, split_length, part_size = ENCODINGS[encoding]
-        encoded_text = encode(text)
+        encoded_text = encode(text) """
 
-    if len(encoded_text) > split_length:
-        if use_udhi:
+    
+    if len(encoded_text) > part_size and not isinstance(encoded_text, list):
+        """ if use_udhi:
             # Split the text into well-formed parts.
             esm_class = consts.SMPP_GSMFEAT_UDHI
             # FIXME: 7-bit encoding has variable-length characters.
             # FIXME: it means that a character may be broken by splitting.
-            parts = make_parts_encoded(encoded_text, part_size)
-        else:
-            # We will have to use SaR to send the message
-            esm_class = consts.SMPP_MSGTYPE_DEFAULT
-            parts = split_sequence(encoded_text, part_size)
-            if len(parts) > 255:
-                raise exceptions.MessageTooLong()
-    else:
+            parts = make_parts_encoded(part, part_size)
+        else: """
+        # We will have to use SaR to send the message
+        esm_class = consts.SMPP_MSGTYPE_DEFAULT
+        parts = split_sequence(encoded_text, part_size)
+        if len(parts) > 255:
+            raise exceptions.MessageTooLong()
+    elif not isinstance(encoded_text, list):
+        print("Message fits into a single part")
         # Normal message.
         esm_class = consts.SMPP_MSGTYPE_DEFAULT
         parts = [encoded_text]
+    else:
+        print("Mensaje fragmentado según se indicó por usuario")
+        # Normal message.
+        esm_class = consts.SMPP_MSGTYPE_DEFAULT
+        parts = encoded_text
 
     return parts, encoding, esm_class
 
@@ -68,6 +82,7 @@ ENCODINGS = {
     consts.SMPP_ENCODING_DEFAULT: (gsm_encode, consts.SEVENBIT_LENGTH, consts.SEVENBIT_PART_SIZE),
     consts.SMPP_ENCODING_ISO88591: (lambda text: text.encode('iso-8859-1'), consts.EIGHTBIT_LENGTH, consts.EIGHTBIT_PART_SIZE),
     consts.SMPP_ENCODING_ISO10646: (lambda text: text.encode('utf-16-be'), consts.UCS2_LENGTH, consts.UCS2_PART_SIZE),
+    consts.SMPP_ENCODING_BINARY2: (lambda text: bytes(int(text[i:i+8], 2) for i in range(0, len(text), 8)), consts.UCS2_LENGTH, consts.UCS2_PART_SIZE),
 }
 
 
